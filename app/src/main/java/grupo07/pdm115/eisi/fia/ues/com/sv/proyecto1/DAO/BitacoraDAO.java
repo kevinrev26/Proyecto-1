23120d4b38c9f38ecf9395ea385f3f442511a590
GabrielloVaCh <sk8jaime@hotmail.com>
2016-05-07 22:25:32 -0600

    Commin con git --all XD

---
+++ app/src/main/java/grupo07/pdm115/eisi/fia/ues/com/sv/proyecto1/DAO/BitacoraDAO.java
@@ -0,0 +1,10 @@
+package grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.DAO;
+
+import java.util.ArrayList;
+
+import grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.Modelo.Bitacora;
+/**
+ * Created by Marvin on 07/05/2016.
+ */
+public class BitacoraDAO {
+}
---
+++ app/src/main/java/grupo07/pdm115/eisi/fia/ues/com/sv/proyecto1/Modelo/Bitacora.java
@@ -0,0 +1,7 @@
+package grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.Modelo;
+
+/**
+ * Created by Marvin on 07/05/2016.
+ */
+public class Bitacora {
+}
