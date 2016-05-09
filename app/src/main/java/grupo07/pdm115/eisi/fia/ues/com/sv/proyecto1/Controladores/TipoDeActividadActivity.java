5bb84e9f3a5ddfee130cad2cb09074ac496af062
GabrielloVaCh <sk8jaime@hotmail.com>
2016-05-08 23:56:34 -0600

    Implementacion de clase TipoActividadDAO

--- app/src/main/java/grupo07/pdm115/eisi/fia/ues/com/sv/proyecto1/Controladores/TipoDeActividadActivity.java
+++ app/src/main/java/grupo07/pdm115/eisi/fia/ues/com/sv/proyecto1/Controladores/TipoDeActividadActivity.java
@@ -1,15 +1,73 @@
 package grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.Controladores;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.ArrayList;
+
+import grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.DAO.TipoDeActividadDAO;
+import grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.Modelo.TipoDeActividad;
 import grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.R;
 
-public class TipoDeActividadActivity extends AppCompatActivity {
+public class TipoDeActividadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //TAG de la clase
+    public static final String TAG = "TipoDeActividadActivity";
+    private ArrayList<TipoDeActividad> tipoActividad = null;
+
+    //Botones para controlar la app
+    private Button btnLlenarBD, btnAgregar, btnActualizar, btnEliminar, btnLeer;
+    private TipoDeActividadDAO mTipoDeActividadDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_de_actividad);
+
+        //Intanciando el DAO
+        mTipoDeActividadDAO= new TipoDeActividadDAO(TipoDeActividadActivity.this);
+        Log.i("DAO", "Metodo on Create");
+
+        //EnlazarWidgets
+        enlazarWidgets();
+        //Establecer los listeners
+        setListeners();
     }
+
+    private void enlazarWidgets(){
+        btnLlenarBD = (Button) findViewById(R.id.btnLlenarDB);
+        btnActualizar = (Button) findViewById(R.id.btnActualizar);
+        //btnAgregar = (Button) findViewById(R.id.btnAgregarTipoActividad);
+        btnEliminar = (Button) findViewById(R.id.btnEliminar);
+        btnLeer = (Button) findViewById(R.id.btnSeleccionar);
+    }
+
+    private void setListeners(){
+        btnLeer.setOnClickListener(this);
+        btnEliminar.setOnClickListener(this);
+        btnAgregar.setOnClickListener(this);
+        btnActualizar.setOnClickListener(this);
+        btnLlenarBD.setOnClickListener(this);
+    }
+
+    private void cargarTipoActividad()
+    {
+        tipoActividad = mTipoDeActividadDAO.getListaTiposActividad();
+    }
+
+    @Override
+    public void onClick(View v){
+        Intent intent=null;
+        switch (v.getId()){
+            case R.id.btnLlenarDB:
+               // llenarDB();
+                break;
+            //case R.id.
+        }
+    }
+
 }
--- app/src/main/java/grupo07/pdm115/eisi/fia/ues/com/sv/proyecto1/DAO/TipoDeActividadDAO.java
+++ app/src/main/java/grupo07/pdm115/eisi/fia/ues/com/sv/proyecto1/DAO/TipoDeActividadDAO.java
@@ -1,7 +1,9 @@
 package grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.DAO;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import grupo07.pdm115.eisi.fia.ues.com.sv.proyecto1.Modelo.TipoDeActividad;
  */
 public class TipoDeActividadDAO  extends MasterDAO
 {
+    //Constantes para las tablas
     private static final String TIPOACTIVIDAD_TABLE = "tipo_actividad";
     private static final String ID_TIPO_ACTIVIDAD = "id_tipo_actividad";
     private static final int ID_COL = 0;
@@ -22,11 +25,102 @@ public class TipoDeActividadDAO  extends MasterDAO
     private static final String DESCRIPCION = "descripcion";
     private static final int DESCRIP_COL = 3;
 
-    
+
     public TipoDeActividadDAO(Context ctx)
     {
         super(ctx);
         Log.i("DAO", "Dentro del constructor TipoDeActividad DAO");
     }
 
+    /** Con este metodo se construye el String para la creacion de la tabla*/
+
+    public static  String crearTablaTipoDeActividad()
+    {
+        return "CREATE TABLE" + TIPOACTIVIDAD_TABLE + "(" +
+                ID_TIPO_ACTIVIDAD + "TEXT(5) NOT NULL, " +
+                NOMBRE_ACTIVIDAD + "TEXT (10) NOT NULL, " +
+                HORAS + "INTEGER(2) NOT NULL, " +
+                DESCRIPCION + "TEXT(5) , " +
+                "PRIMARY KEY(" +ID_TIPO_ACTIVIDAD +") " +
+                ");" ;
+    }
+
+    public static String eliminarTablaDeActividad(){
+        return "DROP TABLE IF EXISTS " + TIPOACTIVIDAD_TABLE;
+    }
+
+    public long insertarTipoActividad(TipoDeActividad tipoActividad){
+        ContentValues contentValues= new ContentValues();
+
+        contentValues.put(ID_TIPO_ACTIVIDAD,tipoActividad.getId_tipo_actividad());
+        contentValues.put(NOMBRE_ACTIVIDAD,tipoActividad.getNombre_actividad());
+        contentValues.put(String.valueOf(HORAS),tipoActividad.getCantidad_horas()); //dudas ahi ?????
+        contentValues.put(DESCRIPCION,tipoActividad.getDescripcion());
+
+        return mDatabase.insert(TIPOACTIVIDAD_TABLE,null,contentValues);
+    }
+
+    public int actualizarTipoActividad(TipoDeActividad tipoActividad){
+        ContentValues contentValues= new ContentValues();
+
+        contentValues.put(ID_TIPO_ACTIVIDAD,tipoActividad.getId_tipo_actividad());
+        contentValues.put(NOMBRE_ACTIVIDAD,tipoActividad.getNombre_actividad());
+        contentValues.put(String.valueOf(HORAS),tipoActividad.getCantidad_horas()); //dudas ahi ???????
+        contentValues.put(DESCRIPCION,tipoActividad.getDescripcion());
+
+        String where=ID_TIPO_ACTIVIDAD + "= ?";
+        String[] whereArgs = {tipoActividad.getId_tipo_actividad() };
+        return mDatabase.update(TIPOACTIVIDAD_TABLE, contentValues, where, whereArgs);
+    }
+
+    public int eliminarTipoActividad(String id_tipo_actividad){
+        String where = ID_TIPO_ACTIVIDAD + "= ?";
+        String[] whereArgs={id_tipo_actividad};
+
+        return mDatabase.delete(TIPOACTIVIDAD_TABLE, where, whereArgs);
+    }
+
+    public ArrayList<TipoDeActividad> getListaTiposActividad(){
+        String query = "SELECT * FROM " + TIPOACTIVIDAD_TABLE;
+        Cursor cursor = mDatabase.rawQuery(query,null);
+
+        ArrayList<TipoDeActividad> tiposActividades = new ArrayList<>();
+        while (cursor.moveToNext()){
+            tiposActividades.add(getTiposActividadesPorCursor(cursor));
+        }
+        if (cursor!=null){
+            cursor.close();
+        }
+        return tiposActividades;
+    }
+
+    public TipoDeActividad getTipoDeActividad(String id_tipo_actividad){
+        String where = ID_TIPO_ACTIVIDAD + "= ?";
+        String whereArgs[] = {id_tipo_actividad};
+
+        Cursor cursor=mDatabase.query(TIPOACTIVIDAD_TABLE, null, where, whereArgs, null, null, null);
+        cursor.moveToFirst();
+        TipoDeActividad temp = getTiposActividadesPorCursor(cursor);
+        if (cursor!=null){
+            cursor.close();
+        }
+        return temp;
+    }
+
+    private static TipoDeActividad getTiposActividadesPorCursor(Cursor cursor){
+        if (cursor==null || cursor.getCount()==0){
+            return null;
+        }else {
+            try {
+                return new TipoDeActividad(
+                        cursor.getString(ID_COL),
+                        cursor.getString(NOMBRE_COL),
+                        cursor.getInt(HORAS_COL), //dudas ?????
+                        cursor.getString(DESCRIP_COL));
+            }catch (Exception e){
+                Log.i("EXE",e.toString());
+                return null;
+            }
+        }
+    }
 }
